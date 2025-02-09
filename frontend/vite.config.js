0ca61542791820179b2d9a0813e8f5b96f69f0c6
Nashit Budhwani <NBudhwani@blueoceansp.ai>
2025-02-09 17:47:17 +0500

    build done

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,6 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [react(),],
+  base: '/Dropshipr/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
