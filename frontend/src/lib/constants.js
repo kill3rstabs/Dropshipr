4cf60ce31b3f41dfc44605d54db42e39362eb6f4
Nashit Budhwani <NBudhwani@blueoceansp.ai>
2025-08-22 15:11:45 +0500

    refactor: Change IS_DEV variable to a constant for better code clarity and consistency

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,4 +1,4 @@
-IS_DEV = false;
+const IS_DEV = false;
 
 // if is dev then url is http://localhost:8000/api else /api only
 export const API_BASE_URL = IS_DEV ? "http://localhost:8000/api" : "/api";
